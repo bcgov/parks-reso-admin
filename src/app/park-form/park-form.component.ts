1ba36a64866ddbdd02c5406db963d2a6c31cd8a8
Daniel Truong <daniel@dtsoftware.ca>
2021-05-27 10:19:03 -0700

    Lint

--- src/app/park-form/park-form.component.ts
+++ src/app/park-form/park-form.component.ts
@@ -18,14 +18,14 @@ export class ParkFormComponent implements OnInit {
     parkDescription: 'Massive trees, majestic waterfalls, a meandering river that meets the sea, flowers, birds and fascinating fish are but a few of the attractions that draw people to Goldstream Provincial Park, a mere 16 km from downtown Victoria on southern Vancouver Island.',
     parkStatus: true,
     parkVisibility: true,
-  }
+  };
 
   public parkForm = new FormGroup({
     parkName: new FormControl('', Validators.required),
     parkDescription: new FormControl(''),
     parkStatus: new FormControl(false),
     parkVisibility: new FormControl(false)
-  })
+  });
 
   constructor(
     private router: Router,
@@ -41,7 +41,7 @@ export class ParkFormComponent implements OnInit {
       parkDescription: this.mockParkObject.parkDescription,
       parkStatus: this.mockParkObject.parkStatus,
       parkVisibility: this.mockParkObject.parkVisibility,
-    })
+    });
   }
 
   toggleAddEdit() {
@@ -49,19 +49,19 @@ export class ParkFormComponent implements OnInit {
     this.resetForm();
   }
 
-  getParkStatusString(){
+  getParkStatusString() {
     return this.parkForm.get('parkStatus').value ? 'Open' : 'Closed';
   }
 
-  getParkVisibilityString(){
+  getParkVisibilityString() {
     return this.parkForm.get('parkVisibility').value ? 'Visible to public' : 'Not visible to public';
   }
 
   submitForm() {
     const message = `<strong>Park Name: </strong>` + this.parkForm.get('parkName').value +
-    `</br><strong>Park Description: </strong>` + this.parkForm.get('parkDescription').value +
-    `</br><strong>Park Status: </strong>` + this.getParkStatusString() +
-    `</br><strong>Park Visibility: </strong>` + this.getParkVisibilityString();
+      `</br><strong>Park Description: </strong>` + this.parkForm.get('parkDescription').value +
+      `</br><strong>Park Status: </strong>` + this.getParkStatusString() +
+      `</br><strong>Park Visibility: </strong>` + this.getParkVisibilityString();
 
     this.dialogService
       .addDialog(
@@ -76,7 +76,7 @@ export class ParkFormComponent implements OnInit {
         if (result) {
           this.router.navigate(['parks']);
         }
-      })
+      });
   }
 
   cancel() {
