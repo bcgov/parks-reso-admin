edacea7a973ea12dc5dde7074544dd49824d3415
Daniel Truong <daniel@dtsoftware.ca>
2021-09-07 14:18:55 -0700

    Updating viewer_protocol_policy

--- terraform/src/cloudfront.tf
+++ terraform/src/cloudfront.tf
@@ -100,7 +100,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
