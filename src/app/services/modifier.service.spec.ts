09613bc05c17dd060be62d57f3841ab8d1de0770
Daniel Truong <daniel@digitalspace.ca>
2022-11-07 18:45:05 +0000

    Fixing tests

--- src/app/services/modifier.service.spec.ts
+++ src/app/services/modifier.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ConfigService } from './config.service';
 
 import { ModifierService } from './modifier.service';
 
@@ -6,7 +8,9 @@ describe('ModifierService', () => {
   let service: ModifierService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, ConfigService],
+    });
     service = TestBed.inject(ModifierService);
   });
 
