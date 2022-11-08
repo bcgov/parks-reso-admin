c9fc9f1b03c65b5c5819464d09bbb138f231c3bc
Daniel Truong <daniel@digitalspace.ca>
2022-11-08 23:49:29 +0000

    Fixing tests

--- src/app/resolvers/facility-details.resolver.spec.ts
+++ src/app/resolvers/facility-details.resolver.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ConfigService } from '../services/config.service';
 
 import { FacilityDetailsResolver } from './facility-details.resolver';
 
@@ -6,7 +8,9 @@ describe('FacilityDetailsResolver', () => {
   let resolver: FacilityDetailsResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, ConfigService],
+    });
     resolver = TestBed.inject(FacilityDetailsResolver);
   });
 
