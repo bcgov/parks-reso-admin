c8b1bd5bebae48d7ab46fc19b5cf0bc3da9ca4b2
Daniel Truong <daniel@digitalspace.ca>
2022-11-11 02:16:16 +0000

    Fixing tests

--- src/app/shared/components/fancy-header/fancy-header.component.spec.ts
+++ src/app/shared/components/fancy-header/fancy-header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FancyHeaderComponent } from './fancy-header.component';
 
@@ -8,9 +9,9 @@ describe('FancyHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FancyHeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [FancyHeaderComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FancyHeaderComponent);
     component = fixture.componentInstance;
