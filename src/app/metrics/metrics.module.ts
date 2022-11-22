579d374f4df94e761b5105b7733c0bd0d8793a0e
meredithom <meredith@digitalspace.ca>
2022-11-22 11:32:05 -0800

    BRS-893: Metrics page added (#159)

---
+++ src/app/metrics/metrics.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MetricsComponent } from './metrics.component';
+
+@NgModule({
+  declarations: [MetricsComponent],
+  imports: [CommonModule],
+  providers: [],
+})
+export class MetricsModule {}
