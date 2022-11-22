c067e5516466fe386e1f6051d7c8bda30c2751a8
Mark Lisé <mark@digitalspace.ca>
2022-11-22 08:19:06 -0800

    BRS-915: DUP Admin - Remove "cancel passes" button when not sysadmin

--- src/app/parks-management/facility-details/passes-list/passes-list.component.ts
+++ src/app/parks-management/facility-details/passes-list/passes-list.component.ts
@@ -14,6 +14,7 @@ import { tableSchema } from 'src/app/shared/components/table/table.component';
 import { Constants } from 'src/app/shared/utils/constants';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { modalSchema } from 'src/app/shared/components/modal/modal.component';
+import { KeycloakService } from 'src/app/services/keycloak.service';
 
 @Component({
   selector: 'app-passes-list',
@@ -34,6 +35,7 @@ export class PassesListComponent implements OnInit, OnDestroy {
 
   constructor(
     protected dataService: DataService,
+    protected keyCloakService: KeycloakService,
     protected passService: PassService,
     protected modalService: BsModalService,
     protected vcr: ViewContainerRef
@@ -59,6 +61,10 @@ export class PassesListComponent implements OnInit, OnDestroy {
       return true;
     }
     return false;
+  }
+
+  isAllowed(method) {
+    return this.keyCloakService.isAllowed(method);
   }
 
   displayPassModal(passObj) {
@@ -135,6 +141,66 @@ export class PassesListComponent implements OnInit, OnDestroy {
   }
 
   createTable() {
+
+    let columns = [
+      {
+        id: 'passId',
+        displayHeader: 'Reg #',
+        columnClasses: 'ps-3 pe-3',
+        mapValue: (passObj) => passObj.sk,
+      },
+      {
+        id: 'email',
+        displayHeader: 'Email',
+        columnClasses: 'px-3',
+        mapValue: (passObj) => passObj.email,
+      },
+      {
+        id: 'numberOfGuests',
+        displayHeader: 'Guests',
+        columnClasses: 'px-3',
+        mapValue: (passObj) => passObj.numberOfGuests,
+      },
+      {
+        id: 'date',
+        displayHeader: 'Date',
+        columnClasses: 'px-3',
+        mapValue: (passObj) => passObj.shortPassDate,
+      },
+      {
+        id: 'status',
+        displayHeader: 'Status',
+        columnClasses: 'px-3',
+        mapValue: (passObj) => passObj.passStatus,
+      }
+    ];
+
+    if (this.isAllowed('cancel-passes')) {
+      let cancelLayout = {
+        id: 'cancel-button',
+        displayHeader: 'Actions',
+        columnClasses: 'ps-5 pe-3',
+        width: '10%',
+        mapValue: () => null,
+        cellTemplate: (passObj) => {
+          const self = this;
+          return {
+            component: TableButtonComponent,
+            inputs: {
+              altText: 'Cancel',
+              buttonClass: 'btn btn-outline-danger',
+              iconClass: 'bi bi-x-circle-fill',
+              isDisabled: this.disableCancelButton(passObj),
+              onClick: function () {
+                self.displayCancelModal(passObj);
+              },
+            },
+          };
+        },
+      };
+      columns.push(cancelLayout);
+    }
+
     this.tableSchema = {
       id: 'passes-list',
       rowClick: (passObj) => {
@@ -143,60 +209,7 @@ export class PassesListComponent implements OnInit, OnDestroy {
           self.displayPassModal(passObj);
         };
       },
-      columns: [
-        {
-          id: 'passId',
-          displayHeader: 'Reg #',
-          columnClasses: 'ps-3 pe-3',
-          mapValue: (passObj) => passObj.sk,
-        },
-        {
-          id: 'email',
-          displayHeader: 'Email',
-          columnClasses: 'px-3',
-          mapValue: (passObj) => passObj.email,
-        },
-        {
-          id: 'numberOfGuests',
-          displayHeader: 'Guests',
-          columnClasses: 'px-3',
-          mapValue: (passObj) => passObj.numberOfGuests,
-        },
-        {
-          id: 'date',
-          displayHeader: 'Date',
-          columnClasses: 'px-3',
-          mapValue: (passObj) => passObj.shortPassDate,
-        },
-        {
-          id: 'status',
-          displayHeader: 'Status',
-          columnClasses: 'px-3',
-          mapValue: (passObj) => passObj.passStatus,
-        },
-        {
-          id: 'cancel-button',
-          displayHeader: 'Actions',
-          columnClasses: 'ps-5 pe-3',
-          width: '10%',
-          mapValue: () => null,
-          cellTemplate: (passObj) => {
-            const self = this;
-            return {
-              component: TableButtonComponent,
-              inputs: {
-                altText: 'Cancel',
-                buttonClass: 'btn btn-outline-danger',
-                iconClass: 'bi bi-x-circle-fill',
-                isDisabled: this.disableCancelButton(passObj),
-                onClick: function () {
-                  self.displayCancelModal(passObj);
-                },
-              },
-            };
-          },
-        },
-      ],
+      columns: columns
     };
   }
 
