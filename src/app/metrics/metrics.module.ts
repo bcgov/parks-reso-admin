f66d913649497ea757f04c7400f4b5d26145bcee
Wilson Wong <wilwong89@gmail.com>
2023-02-21 09:11:59 -0800

    BRS-1033: Use new metrics component for doughnut chart

--- src/app/metrics/metrics.module.ts
+++ src/app/metrics/metrics.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MetricsComponent } from './metrics.component';
+import { SharedMetricsModule } from '../shared/components/metrics/shared-metrics.module';
 
 @NgModule({
   declarations: [MetricsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedMetricsModule],
   providers: [],
 })
 export class MetricsModule {}
