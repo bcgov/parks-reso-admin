3f4a990c44c18f339d517f88683ae63aa251bd71
cameron <cameron@digitalspace.ca>
2023-08-04 09:38:51 -0700

    BRS-1228 adding checkedIn status to csv export

--- src/app/services/metrics.service.ts
+++ src/app/services/metrics.service.ts
@@ -115,31 +115,38 @@ export class MetricsService {
       // This may have to change later.
       const parkName = this.dataService.getItemValue(Constants.dataIds.PARK_AND_FACILITY_LIST)[park].name;
       const dateInterval = this.utils.createShortDateInterval(dateRange[0], dateRange[1]);
-      let content = [['Park', 'Facility', 'Date', 'Status', 'AM base capacity', 'AM capacity modifier', 'AM total passes', 'AM booked passes', 'AM cancellations', 'PM base capacity', 'PM capacity modifier', 'PM total passes', 'PM booked passes', 'PM cancellations', 'All-Day base capacity', 'All-Day capacity modifier', 'All-Day total passes', 'All-Day booked passes', 'All-Day cancellations']];
+      let content = [['Park', 'Facility', 'Date', 'Status', 'AM base capacity', 'AM capacity modifier', 'AM total passes', 'AM booked passes', 'AM checked-in', 'AM cancellations', 'PM base capacity', 'PM capacity modifier', 'PM total passes', 'PM booked passes', 'PM checked-in', 'PM cancellations', 'All-Day base capacity', 'All-Day capacity modifier', 'All-Day total passes', 'All-Day booked passes', 'All-day checked-in', 'All-Day cancellations']];
       for (const date of dateInterval) {
         let metric = metricsList.find((item) => item.sk === date);
+        console.log('metric:', metric);
         content.push([parkName, facility, date,
           'N/A', // TODO: update metrics to include open/closed status
+          // AM
           metric?.capacities?.AM?.baseCapacity ?? 'N/A',
           metric?.capacities?.AM?.capacityModifier ?? 'N/A',
           isNaN(metric?.capacities?.AM?.baseCapacity + metric?.capacities?.AM?.capacityModifier) ?
             'N/A' : metric?.capacities?.AM?.baseCapacity + metric?.capacities?.AM?.capacityModifier,
           isNaN(metric?.capacities?.AM?.baseCapacity + metric?.capacities?.AM?.capacityModifier - metric?.capacities?.AM?.availablePasses) ?
             'N/A' : metric?.capacities?.AM?.baseCapacity + metric?.capacities?.AM?.capacityModifier - metric?.capacities?.AM?.availablePasses,
+          metric?.capacities?.AM?.checkedIn ?? 'N/A',
           metric?.capacities?.AM?.passStatuses?.cancelled ?? 'N/A',
+          // PM
           metric?.capacities?.PM?.baseCapacity ?? 'N/A',
           metric?.capacities?.PM?.capacityModifier ?? 'N/A',
           isNaN(metric?.capacities?.PM?.baseCapacity + metric?.capacities?.PM?.capacityModifier) ?
             'N/A' : metric?.capacities?.PM?.baseCapacity + metric?.capacities?.PM?.capacityModifier,
           isNaN(metric?.capacities?.PM?.baseCapacity + metric?.capacities?.PM?.capacityModifier - metric?.capacities?.PM?.availablePasses) ?
             'N/A' : metric?.capacities?.PM?.baseCapacity + metric?.capacities?.PM?.capacityModifier - metric?.capacities?.PM?.availablePasses,
+          metric?.capacities?.PM?.checkedIn ?? 'N/A',
           metric?.capacities?.PM?.passStatuses?.cancelled ?? 'N/A',
+          // All day
           metric?.capacities?.DAY?.baseCapacity ?? 'N/A',
           metric?.capacities?.DAY?.capacityModifier ?? 'N/A',
           isNaN(metric?.capacities?.DAY?.baseCapacity + metric?.capacities?.DAY?.capacityModifier) ?
             'N/A' : metric?.capacities?.DAY?.baseCapacity + metric?.capacities?.DAY?.capacityModifier,
           isNaN(metric?.capacities?.DAY?.baseCapacity + metric?.capacities?.DAY?.capacityModifier - metric?.capacities?.DAY?.availablePasses) ?
             'N/A' : metric?.capacities?.DAY?.baseCapacity + metric?.capacities?.DAY?.capacityModifier - metric?.capacities?.DAY?.availablePasses,
+          metric?.capacities?.DAY?.checkedIn ?? 'N/A',
           metric?.capacities?.DAY?.passStatuses?.cancelled ?? 'N/A',
         ]);
       }
